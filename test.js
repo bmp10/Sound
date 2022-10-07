81b84315fa7cb0943182fdb496618563aa696d56
bmp10 <89558241+bmp10@users.noreply.github.com>
2022-10-06 22:06:17 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,28 @@
-let mic, fft;
+let mic = {}
+let fft = {}
 
 function setup() {
-    setInterval(() => {
-        createCanvas(710, 400);
-        noFill();
+    createCanvas(710, 400);
+    noFill();
+
+    mic = new p5.AudioIn();
+    mic.start();
     
-        mic = new p5.AudioIn();
-        mic.start();
+    setInterval(() => {
+        mic.resume()
         fft = new p5.FFT();
         fft.setInput(mic);
     }, 1000)
 }
 
 function draw() {
-    try {
-        background(200);
-    
-        let spectrum = fft.analyze();
-    
-        beginShape();
-        for (i = 0; i < spectrum.length; i++) {
-            vertex(i, map(spectrum[i], 0, 255, height, 0));
-        }
-        endShape();
-    } catch {}
+    background(200);
+
+    let spectrum = fft.analyze();
+
+    beginShape();
+    for (i = 0; i < spectrum.length; i++) {
+        vertex(i, map(spectrum[i], 0, 255, height, 0));
+    }
+    endShape();
 }
